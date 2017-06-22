8bd9f0b542119e481df3d0121c6eb38b42d3967c
peknight <peknight@qq.com>
2017-06-22 13:27:42 +0800

    Add Inherited to CommonLog

--- src/main/java/com/peknight/common/logging/CommonLog.java
+++ src/main/java/com/peknight/common/logging/CommonLog.java
@@ -2,19 +2,21 @@ package com.peknight.common.logging;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 通用日志注解
+ * Common Log Annotation
  *
  * @author PeKnight
  *
  * Created by PeKnight on 2017/6/21.
  */
-@Target({ElementType.METHOD, ElementType.TYPE, ElementType.PACKAGE})
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
 public @interface CommonLog {
 

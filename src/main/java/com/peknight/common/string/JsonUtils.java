a3579ac0d19fd1130c60e2d41769864b6812285d
peknight <peknight@qq.com>
2017-07-31 11:38:01 +0800

    Add JsonUtils

---
+++ src/main/java/com/peknight/common/string/JsonUtils.java
@@ -0,0 +1,58 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017-2027 PeKnight(JKpeknight@gmail.com)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.peknight.common.string;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ *
+ *
+ * @author PeKnight
+ *
+ * Created by PeKnight on 2017/7/31.
+ */
+public final class JsonUtils {
+    private static final ObjectMapper OBJECT_MAPPER;
+
+    private JsonUtils() {}
+
+    static {
+        OBJECT_MAPPER = new ObjectMapper();
+        OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        OBJECT_MAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+    }
+
+    public static <T> String write(T value) throws JsonProcessingException {
+        return OBJECT_MAPPER.writeValueAsString(value);
+    }
+
+    public static <T> T read(String json, Class<T> tClass) throws IOException {
+        return OBJECT_MAPPER.readValue(json, tClass);
+    }
+}

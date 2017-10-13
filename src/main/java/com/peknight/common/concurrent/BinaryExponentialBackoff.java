43bec4958736b52390b4dd335b8eb47f9a9b83ad
peknight <peknight@qq.com>
2017-10-13 20:42:23 +0800

    改进二进制指数退避算法类

--- src/main/java/com/peknight/common/concurrent/BinaryExponentialBackoff.java
+++ src/main/java/com/peknight/common/concurrent/BinaryExponentialBackoff.java
@@ -23,7 +23,7 @@
  */
 package com.peknight.common.concurrent;
 
-import com.peknight.common.util.function.Function;
+import com.peknight.common.function.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/com/peknight/common/function/Function.java
+++ src/main/java/com/peknight/common/function/Function.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.peknight.common.util.function;
+package com.peknight.common.function;
 
 /**
  *
